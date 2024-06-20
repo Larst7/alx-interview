fe3684a8bcf8c5a9303a478061236572a18f3cd5
Larst7 <lasgwenzi7@gmail.com>
2024-06-20 20:47:29 +0200

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,12 +5,12 @@ import sys
 
 def print_msg(dict_sc, total_file_size):
     """
-    method to print
+    Method to print
     Args:
         dict_sc: dict of status codes
         total_file_size: total of the file
     Returns:
-       Nothing
+        Nothing
     """
 
     print("File size: {}".format(total_file_size))
@@ -23,32 +23,32 @@ total_file_size = 0
 code = 0
 counter = 0
 dict_sc = {"200": 0,
-           "301": 0;
+           "301": 0,
            "400": 0,
            "401": 0,
            "403": 0,
            "404": 0,
            "405": 0,
-           "500": 0,
+           "500": 0}
 
 try:
-    for line in sys.stdout:
+    for line in sys.stdin:
         parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1] # inverting
+        parsed_line = parsed_line[::-1]  # inverting
 
         if len(parsed_line) > 2:
             counter += 1
 
             if counter <= 10:
-                total_file_size += int(parsed_line[0] #file size
-                code = parsed_line[1] #status code
+                total_file_size += int(parsed_line[0])  # file size
+                code = parsed_line[1]  # status code
 
                 if (code in dict_sc.keys()):
                     dict_sc[code] += 1
 
-            if(counter == 10):
+            if (counter == 10):
                 print_msg(dict_sc, total_file_size)
                 counter = 0
 
 finally:
-    print_msg(dit_sc, total_file_size)
+    print_msg(dict_sc, total_file_size)
