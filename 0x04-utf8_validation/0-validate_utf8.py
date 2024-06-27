4d38f46e908e3bff0e7d16f2a66bfe4fab1ed214
Larst7 <lasgwenzi7@gmail.com>
2024-06-27 22:57:10 +0200

    Add UTF-8 validation function to ensure all checks passes

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """utf-8 validation module"""
 
+
 def validUTF8(data):
     """
     Determine if the given data set represents a valid UTF-8 encoding.
@@ -14,38 +15,22 @@ def validUTF8(data):
     num_bytes = 0
 
     for byte in data:
-        # Mask the byte to get the least significant 8 bits
         byte = byte & 0xFF
 
         if num_bytes == 0:
-            # Determine the number of bytes in the character
             if byte >> 5 == 0b110:
                 num_bytes = 1
             elif byte >> 4 == 0b1110:
                 num_bytes = 2
             elif byte >> 3 == 0b11110:
                 num_bytes = 3
-            elif byte >> 7 == 0:
+            elif byte >> 7 == 0b0:
                 continue
             else:
                 return False
         else:
-            # Check continuation byte (should start with '10')
             if byte >> 6 != 0b10:
                 return False
             num_bytes -= 1
 
-    # If num_bytes is not zero, then we have an incomplete character
     return num_bytes == 0
-
-# Test cases (to be removed or commented out in production)
-if __name__ == "__main__":
-    data1 = [65]
-    print(validUTF8(data1))  # True
-
-    data2 = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
-    print(validUTF8(data2))  # True
-
-    data3 = [229, 65, 127, 256]
-    print(validUTF8(data3))  # False
-
