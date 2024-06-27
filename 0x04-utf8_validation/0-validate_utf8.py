ebaa8e1228dcad5bc85a224e659a60b7f4adde6e
Larst7 <lasgwenzi7@gmail.com>
2024-06-27 22:51:57 +0200

    Add UTF-8 validation function to pass all checks

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -13,40 +13,36 @@ def validUTF8(data):
     Returns:
         bool: True if data is a valid UTF-8 encoding, else False.
     """
-    # Number of bytes in the current UTF-8 character
     num_bytes = 0
-    
-    # Masks to check the leading bits of a byte
-    masks = [0b10000000, 0b11100000, 0b11110000, 0b11111000]
-    # Expected patterns for leading bits for characters of different lengths
-    patterns = [0b00000000, 0b11000000, 0b11100000, 0b11110000]
-    
+
     for byte in data:
-        # Check the first byte
+        # Ensure byte is in the range of a valid byte
+        if byte < 0 or byte > 255:
+            return False
+
         if num_bytes == 0:
-            # Determine the number of bytes in this character
-            if (byte & masks[0]) == 0:
+            if (byte >> 5) == 0b110:
                 num_bytes = 1
-            elif (byte & masks[1]) == patterns[1]:
+            elif (byte >> 4) == 0b1110:
                 num_bytes = 2
-            elif (byte & masks[2]) == patterns[2]:
+            elif (byte >> 3) == 0b11110:
                 num_bytes = 3
-            elif (byte & masks[3]) == patterns[3]:
-                num_bytes = 4
-            else:
+            elif (byte >> 7) != 0:
                 return False
-            
-            # If this character needs more bytes, check the continuation bytes
-            if num_bytes > 1:
-                num_bytes -= 1
-            else:
-                num_bytes = 0
         else:
-            # Check continuation byte
-            if (byte & masks[1]) != 0b10000000:
+            if (byte >> 6) != 0b10:
                 return False
             num_bytes -= 1
-    
-    # If num_bytes is not zero, it means we expected more continuation bytes
+
     return num_bytes == 0
+
+# Test cases (to be removed or commented out in production)
+data1 = [65]
+print(validUTF8(data1))  # True
+
+data2 = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
+print(validUTF8(data2))  # True
+
+data3 = [229, 65, 127, 256]
+print(validUTF8(data3))  # False
 
