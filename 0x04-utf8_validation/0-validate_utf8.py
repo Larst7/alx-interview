b465cc66bfddf03a3b171e9d50088f6994d31131
Larst7 <lasgwenzi7@gmail.com>
2024-06-27 22:54:21 +0200

    Add UTF-8 validation function to pass all checks

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""
-UTF-8 Validation
-"""
+"""utf-8 validation module"""
 
 def validUTF8(data):
     """
-    Determines if a given data set represents a valid UTF-8 encoding.
+    Determine if the given data set represents a valid UTF-8 encoding.
 
     Args:
-        data (list): List of integers representing the data bytes.
+        data (List[int]): The list of integers representing the data.
 
     Returns:
         bool: True if data is a valid UTF-8 encoding, else False.
@@ -16,33 +14,23 @@ def validUTF8(data):
     num_bytes = 0
 
     for byte in data:
-        # Ensure byte is in the range of a valid byte
-        if byte < 0 or byte > 255:
-            return False
+        byte = byte & 0xFF
 
         if num_bytes == 0:
-            if (byte >> 5) == 0b110:
+            if byte >> 5 == 0b110:
                 num_bytes = 1
-            elif (byte >> 4) == 0b1110:
+            elif byte >> 4 == 0b1110:
                 num_bytes = 2
-            elif (byte >> 3) == 0b11110:
+            elif byte >> 3 == 0b11110:
                 num_bytes = 3
-            elif (byte >> 7) != 0:
+            elif byte >> 7 == 0b0:
+                continue
+            else:
                 return False
         else:
-            if (byte >> 6) != 0b10:
+            if byte >> 6 != 0b10:
                 return False
             num_bytes -= 1
 
     return num_bytes == 0
-
-# Test cases (to be removed or commented out in production)
-data1 = [65]
-print(validUTF8(data1))  # True
-
-data2 = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
-print(validUTF8(data2))  # True
-
-data3 = [229, 65, 127, 256]
-print(validUTF8(data3))  # False
 
